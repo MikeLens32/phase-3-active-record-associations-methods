d8f23ce27705d15ab9163190ebc54b2b2ecf25fb
Michael Dwyer <Mike.a.dwyer32@gmail.com>
2022-03-17 13:04:09 -0400

    Fin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,27 @@
+puts "Destroying all"
 Song.delete_all
 Genre.delete_all
 Artist.delete_all
 
+puts "Creating songs"
 hotline_bling = Song.create(name: 'Hotline Bling')
 thriller = Song.create(name: 'Thriller')
 
+puts "Creating Artist"
 drake = Artist.create(name: 'Drake')
 mj = Artist.create(name: 'Michael Jackson')
 
+puts "Creating Genre"
 rap = Genre.create(name: 'Rap')
 pop = Genre.create(name: 'Pop')
 
 hotline_bling.artist = drake
 thriller.artist = mj
 
+puts "Shoveling"
 drake.songs << hotline_bling
 mj.songs << thriller
 
+puts "Shoveling"
 pop.songs << thriller
 rap.songs << hotline_bling
